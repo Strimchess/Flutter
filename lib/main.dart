501f57c481b98de4939541581c097c73ef360e2e
Strimchess <slavakozlov545@gmail.com>
2025-11-28 22:15:27 +0300

    Navigation bar design and switch implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:test/generated/assets.dart';
+import 'package:test/screens.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Row Example'
+      home: Layout(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+
+
+class Layout extends StatefulWidget {
+  @override
+  _LayoutState createState() => _LayoutState();
+}
+
+
+
+
+
+class _LayoutState extends State<Layout> {
+
+  int _currentIndex = 0;
+
+
+  final List<Widget> _screens = [
+    MainScreen(),
+    CoachesScreen(),
+    ScheduleScreen(),
+    CalendarScreen(),
+    AccountScreen()
+  ];
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+
+
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.grey,
+        selectedItemColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+            icon: Image(image: NetworkImage('https://img.icons8.com/ios-filled/50/home.png'),
+            width: 35,
+            height: 45,
+            ),
+            label: 'Главная',
+          ),
+          BottomNavigationBarItem(
+            icon: Image(image: NetworkImage('https://img.icons8.com/ios-filled/50/personal-trainer.png'),
+            height: 45,
+            ),
+            label: 'Тренеры',
+          ),
+          BottomNavigationBarItem(
+            icon: Image(image: NetworkImage('https://img.icons8.com/glyph-neue/64/calendar.png'),
+            height: 45,
+            ),
+            label: 'Расписание',
+          ),
+          BottomNavigationBarItem(
+            icon: Image(image: NetworkImage('https://img.icons8.com/ios-glyphs/60/alarm.png'),
+            height: 43,
+            ),
+            label: 'Записи',
+          ),BottomNavigationBarItem(
+            icon: Image(image: NetworkImage('https://img.icons8.com/material-sharp/48/guest-male.png'),
+            height: 45,
+            ),
+            label: 'Аккаунт',
+          ),
+        ],
+      ),
+    );
   }
+
+
 }
---
+++ lib/screens.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:test/generated/assets.dart';
+
+class MainScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello'),
+    );
+  }
+}
+
+class CoachesScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+    );
+  }
+}
+
+class ScheduleScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+    );
+  }
+}
+
+class CalendarScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+    );
+  }
+}
+
+class AccountScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+    );
+  }
+}
